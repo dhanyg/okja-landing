c81db8db7c8a474d9f1b6008493eba74841276c9
Yoga Rama <ramayoga04@gmail.com>
2021-02-07 18:02:48 +0700

    edit package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-    './*.html',
-  ],
+  purge: {
+    enabled: true,
+    content: ['./*.html'],
+  },
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
